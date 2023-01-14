d5d642bd172a8f4cd0203ea5c9cb6e1209954f53
Ross Jackson <ross.santos@gmail.com>
2023-01-14 11:44:32 -0500

    Create left-panel written in react

---
+++ react-left-panel/webpack.config.ts
@@ -0,0 +1,100 @@
+import CopyPlugin from 'copy-webpack-plugin'
+import ESLintPlugin from 'eslint-webpack-plugin'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import path from 'path'
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin'
+import { Configuration } from 'webpack'
+import 'webpack-dev-server'
+
+const isProduction = process.env.NODE_ENV == 'production'
+
+const stylesHandler = isProduction
+    ? MiniCssExtractPlugin.loader
+    : 'style-loader'
+
+const config: Configuration = {
+    entry: './src/index',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+    },
+    devServer: {
+        open: true,
+        host: 'localhost',
+        port: 3010,
+    },
+    plugins: [
+        // new container.ModuleFederationPlugin({
+        //     name: 'Remote',
+        //     filename: 'moduleEntry.js',
+        //     exposes: {
+        //         App: './src/App',
+        //     },
+        //     shared: {
+        //         react: {
+        //             singleton: true,
+        //             requiredVersion: dependencies['react'],
+        //         },
+        //         'react-dom': {
+        //             singleton: true,
+        //             requiredVersion: dependencies['react-dom'],
+        //         },
+        //     },
+        // }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+        new ESLintPlugin({
+            extensions: ['js', 'jsx', 'ts', 'tsx'],
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    context: 'public',
+                    from: '*.{ico,png,json,txt}',
+                },
+            ],
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/i,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(js|jsx)$/i,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: [stylesHandler, 'css-loader'],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
+    resolve: {
+        plugins: [new TsconfigPathsPlugin()],
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    target: 'web',
+}
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production'
+
+        config.plugins?.push(new MiniCssExtractPlugin())
+    } else {
+        config.mode = 'development'
+        config.devtool = 'inline-source-map'
+    }
+    return config
+}
